124d989f2615138a86d4d83a7516dbe25f4d754d
NickBoyadjian <nboyadjian95@gmail.com>
2017-01-03 17:24:32 -0500

    user sign up

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,68 +1,60 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  firebaseApp: Ember.inject.service(),
+    firebaseApp: Ember.inject.service(),
 
-actions: {
+  emailValidation: [{
+    message: 'Invalid email format.',
+    validate: (inputValue) => {
+      let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      return emailPattern.test(inputValue);
+    }
+  }],
 
-  createUser(email, pass) {
-    const auth = this.get('firebaseApp').auth();
-    auth.createUserWithEmailAndPassword(email, pass).then((userResponse) => {
-      const user = this.store.createRecord('user', {
-        id: userResponse.uid,
-        email: userResponse.email
+  actions: {
+    hideEmailMessage(){
+      this.set('emailError', []);
+    },
+
+    hidePassMessage(){
+      this.set('passwordError', []);
+    },
+
+    signUp() {
+      this.set('badEmail', false);
+      this.set('badPassword', false);
+      const auth = this.get('firebaseApp').auth();
+      const email = this.get('email');
+      const pass = this.get('password');
+
+      auth.createUserWithEmailAndPassword(email, pass).then((userResponse) => {
+        console.log('in createUserWithEmailAndPassword function');
+        const user = this.store.createRecord('user', {
+          id: userResponse.uid,
+          email: userResponse.email,
+        });
+        user.save().then(() => {
+          this.get('session').open('firebase', {
+            provider: 'password',
+            email: email,
+            password: pass
+          }).then(() => {
+            this.transitionToRoute('tasks');
+          });
+        });
+      }).catch((error) => {
+        console.log('error: ', error);
+        const errorCode = error.code;
+        if (errorCode === 'auth/email-already-in-use'){
+          this.set('emailError', ['The email address is already in use by another account.']);
+        } else if(errorCode === 'auth/argument-error'){
+          this.set('emailError', ['Email can not be empty']);
+        } else if(errorCode === "auth/invalid-email"){
+          return;
+        } else {
+          this.set('passwordError', ['Password should be at least 6 characters']);
+        }
       });
-      return user.save();
-    });
+    }
   }
-
-}
-
 });
-
-// import Ember from 'ember';
-//
-// export default Ember.Controller.extend({
-//
-//       firebase: Ember.inject.service(),
-//       actions: {
-//           submission: function(){
-//               const ref = this.get('firebase');
-//               ref.createUser({
-//                   email: this.get('email'),
-//                   password: this.get('password')
-//               }, function(error, userData) {
-//
-//               if (error) {
-//
-//                   switch (error.code) {
-//
-//                       case "EMAIL_TAKEN":
-//                           console.log('user already exists');
-//                       break;
-//                       case "INVALID_EMAIL":
-//                           console.log('your email looks wrong');
-//                       break;
-//                       case "INVALID_PASSWORD":
-//                           console.log('your password looks incorrect');
-//                       break;
-//                       default:
-//                           console.log(error.message);
-//
-//                   }
-//
-//               } else {
-//
-//                   console.log('record added!');
-//                   console.log(userData);
-//
-//               }
-//
-//
-//               });
-//
-//           }
-//       }
-//
-//
-// });
